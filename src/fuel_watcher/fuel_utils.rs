8f2049d87a25ad51e9e68b48a3641349f58040a4
VitalyVolozhinov <vitalyvolozhinov@gmail.com>
2023-12-01 15:12:44 +0100

    fixing import

--- src/fuel_watcher/fuel_utils.rs
+++ src/fuel_watcher/fuel_utils.rs
@@ -1,8 +1,6 @@
 use std::sync::Arc;
 use anyhow::{Result, anyhow};
-use fuel_core_client::client::{
-    FuelClient,
-};
+use fuel_core_client::client::FuelClient;
 
 pub async fn setup_fuel_provider(fuels_graphql: &str) -> Result<Arc<FuelClient>> {
     let provider = FuelClient::new(fuels_graphql).unwrap();
